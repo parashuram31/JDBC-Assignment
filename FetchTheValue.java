58b05272289be3f2547f14334a38cbd7c9a2d0df
Parashuram Killiket <parashuramkilliket.sangamone@gmail.com>
2023-06-23 15:40:41 +0530

    JDBC Assignment

--- FetchTheValue.java
+++ FetchTheValue.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class FetchTheValueFromTheDatabase {
+public class FetchTheValue {
 public static void main(String[] args) throws ClassNotFoundException, SQLException {
 	Class.forName("com.mysql.cj.jdbc.Driver");
 	Connection connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/jehm35","root","root");
--- InsertTheValues.java
+++ InsertTheValues.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class InsertTheValuesIntoTheTable {
+public class InsertTheValues {
 	public static void main(String[] args) throws ClassNotFoundException ,SQLException{
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		Connection connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/jehm35","root","root");
